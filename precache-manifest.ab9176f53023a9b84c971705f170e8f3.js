fff82283d28b0862a3ca0f7df9275042308b89c2
Carlos Yoshio Tsuda <cyt231@gmail.com>
2019-08-14 16:01:29 -0300

    Updates

---
+++ precache-manifest.ab9176f53023a9b84c971705f170e8f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ee8f9d5a29f4d84db97cf233f2fcb8",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "10de8ee988cb36511e56",
+    "url": "/burger/static/css/3.98cbc2b3.chunk.css"
+  },
+  {
+    "revision": "13bc7d8515cca3b89282",
+    "url": "/burger/static/css/4.d568ee89.chunk.css"
+  },
+  {
+    "revision": "a80430540a2545d98f83",
+    "url": "/burger/static/css/5.18b11cb8.chunk.css"
+  },
+  {
+    "revision": "42f50f104167ec4fd9d6",
+    "url": "/burger/static/css/main.b6e0e21d.chunk.css"
+  },
+  {
+    "revision": "515618d5ecf9332f8142",
+    "url": "/burger/static/js/2.8e5f6fde.chunk.js"
+  },
+  {
+    "revision": "10de8ee988cb36511e56",
+    "url": "/burger/static/js/3.4243840d.chunk.js"
+  },
+  {
+    "revision": "13bc7d8515cca3b89282",
+    "url": "/burger/static/js/4.a6ae917b.chunk.js"
+  },
+  {
+    "revision": "a80430540a2545d98f83",
+    "url": "/burger/static/js/5.f764d3c4.chunk.js"
+  },
+  {
+    "revision": "42f50f104167ec4fd9d6",
+    "url": "/burger/static/js/main.7c163b18.chunk.js"
+  },
+  {
+    "revision": "253a477ff29c5fcd7433",
+    "url": "/burger/static/js/runtime~main.1d81e635.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/logo.b8503d26.png"
+  }
+]);
